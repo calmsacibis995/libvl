9c6f25cf953d3dfb53c101fcac840a3cfc306b28
Stefanos Stefanidis <www.fe32gr23@gmail.com>
2024-12-24 15:01:40 +0200

    Fix a possible memory leak.

--- src/console.c
+++ src/console.c
@@ -1,17 +1,17 @@
 /*
  * libvl - An library for writing init systems.
- * Copyright (C) 2023 Stefanos Stefanidis, <www.fe32gr23@gmail.com>
- * 
+ * Copyright (C) 2023, 2024 Stefanos Stefanidis, <www.fe32gr23@gmail.com>
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
@@ -71,7 +71,7 @@ console(char *format, ...)
 			 */
 			open_console();
 			setbuf(stdout,&outbuf[0]);
-			
+
 			/*
 			 * Output the message to the console.
 			 */
@@ -79,8 +79,9 @@ console(char *format, ...)
 			fprintf(stdout,"\nINIT: ");
 			vfprintf(stdout, format, va);
 			fflush(stdout);
+			va_end(va);
 			exit(0);
-			
+
 		/*
 		 * The parent waits for the message to complete.
 		 */
@@ -92,8 +93,10 @@ console(char *format, ...)
 	 */
 	} else {
 		setbuf(stdout,&outbuf[0]);
+		va_start(va, format);
 		fprintf(stdout, "\nINIT: ");
 		vfprintf(stdout, format, va);
 		fflush(stdout);
+		va_end(va);
 	}
 }
